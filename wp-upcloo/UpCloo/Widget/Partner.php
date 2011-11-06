fa4b979836138629d464dc14ff49fa9b445c3114
Walter Dal Mut <walter.dalmut@gmail.com>
2011-11-06 14:58:15 +0100

    Added UTM Tagging in VSK widgets

--- wp-upcloo/UpCloo/Widget/Partner.php
+++ wp-upcloo/UpCloo/Widget/Partner.php
@@ -16,13 +16,34 @@ class UpCloo_Widget_Partner
     public function form($instance)
     {
         if ( $instance ) {
-            $title = esc_attr( $instance[ 'upcloo_v_sitekey' ] );
+            $title = esc_attr($instance[ 'upcloo_v_sitekey' ]);
+            $enableUtmTag = $instance['upcloo_v_utm_tag'];
+            $utmCampaign = $instance["upcloo_v_utm_campaign"];
+            $utmMedia = $instance['upcloo_v_utm_media'];
+            $utmSource = $instance['upcloo_v_utm_source'];
         } else {
             $title = __( 'Here your Virtual Site Key', 'text_domain' );
+            $enableUtmTag = __('0', 'text_domain');
+            $utmCampaign = __('', 'text_domain');
+            $utmMedia = __('', text_domain);
+            $utmSource = __('upcloo', text_domain);
         }
         ?>
+
         <label for="<?php echo $this->get_field_id('upcloo_v_sitekey'); ?>"><?php _e('Virtual Partner:'); ?></label> 
         <input class="widefat" id="<?php echo $this->get_field_id('upcloo_v_sitekey'); ?>" name="<?php echo $this->get_field_name('upcloo_v_sitekey'); ?>" type="text" value="<?php echo $title; ?>" />
+        <label for="<?php echo $this->get_field_id('upcloo_v_utm_tag'); ?>"><?php _e('Enable UTM Tagging:'); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('upcloo_v_utm_tag'); ?>" name="<?php echo $this->get_field_name('upcloo_v_utm_tag'); ?>" type="checkbox" <?php echo (($enableUtmTag) ? 'checked' : ''); ?> />
+        
+        <label for="<?php echo $this->get_field_id('upcloo_v_utm_campaign'); ?>"><?php _e('UTM Campaign:'); ?></label> 
+        <input class="widefat" id="<?php echo $this->get_field_id('upcloo_v_utm_campaign'); ?>" name="<?php echo $this->get_field_name('upcloo_v_utm_campaign'); ?>" type="text" value="<?php echo $utmCampaign; ?>" />
+        
+        <label for="<?php echo $this->get_field_id('upcloo_v_utm_media'); ?>"><?php _e('UTM Media:'); ?></label> 
+        <input class="widefat" id="<?php echo $this->get_field_id('upcloo_v_utm_media'); ?>" name="<?php echo $this->get_field_name('upcloo_v_utm_media'); ?>" type="text" value="<?php echo $utmMedia; ?>" />                
+        
+        <label for="<?php echo $this->get_field_id('upcloo_v_utm_source'); ?>"><?php _e('UTM Source:'); ?></label> 
+        <input class="widefat" id="<?php echo $this->get_field_id('upcloo_v_utm_source'); ?>" name="<?php echo $this->get_field_name('upcloo_v_utm_source'); ?>" type="text" value="<?php echo $utmSource; ?>" />
+        
         <?php
     }
 
@@ -30,6 +51,10 @@ class UpCloo_Widget_Partner
     {
         $instance = $old_instance;
         $instance['upcloo_v_sitekey'] = strip_tags($new_instance['upcloo_v_sitekey']);
+        $instance['upcloo_v_utm_tag'] = strip_tags($new_instance['upcloo_v_utm_tag']);
+        $instance['upcloo_v_utm_campaign'] = strip_tags($new_instance['upcloo_v_utm_campaign']);
+        $instance['upcloo_v_utm_media'] = strip_tags($new_instance['upcloo_v_utm_media']);
+        $instance['upcloo_v_utm_source'] = strip_tags($new_instance['upcloo_v_utm_source']);
         return $instance;
     }
 
@@ -40,13 +65,24 @@ class UpCloo_Widget_Partner
             $sitekey = get_option("upcloo_sitekey");
 
             $virtualSiteKey = $instance["upcloo_v_sitekey"];
-
+            
             echo $before_widget;
             if ($virtualSiteKey) {
-                $url = sprintf(UPCLOO_REPOSITORY_END_POINT, $sitekey) . '/' . $virtualSiteKey . "/{$post->post_type}_{$post->ID}.xml"; 
-
+                $url = sprintf(UPCLOO_REPOSITORY_END_POINT, $sitekey) . '/' . $virtualSiteKey . "/{$post->post_type}_{$post->ID}.xml";
+                
                 $datax = upcloo_get_from_repository("", $url);
             }
+            
+            $utmURL = '';
+			if ($instance["upcloo_v_utm_tag"]) {
+
+            	$utmURL = "utm_campaign={$instance["upcloo_v_utm_campaign"]}&utm_medium={$instance["upcloo_v_utm_media"]}&utm_source={$instance["upcloo_v_utm_source"]}";
+               	if (strpos($url, "?") === false) {
+               		$utmURL .= "?{$utmURL}";
+				} else {
+					$utmURL .= "&{$utmURL}";
+				}
+			}
 ?>
     <li class="widget-container widget_upcloo">
         <h3 class="widget-title">Related</h3>
@@ -56,7 +92,7 @@ class UpCloo_Widget_Partner
             if ($datax->doc) :
                 foreach ($datax->doc as $doc):
 ?>
-<li><a href="<?php echo $doc->url?>" target="_blank"><?php echo $doc->title; ?></a></li>
+<li><a href="<?php echo $doc->url . $utmURL?>" target="_blank"><?php echo $doc->title; ?></a></li>
             <?php
                 endforeach;
             endif;
