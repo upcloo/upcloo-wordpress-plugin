d329672fc4f8f169fe772488c7bc47b83e12e9e7
Walter Dal Mut <walter.dalmut@gmail.com>
2011-11-26 17:33:49 +0100

    OPEN - task 8: Link templates http://github.com/wdalmut/upcloo-wordpress-plugin/issues/issue/8
    Configs

--- wp-upcloo/general-options.php
+++ wp-upcloo/general-options.php
@@ -210,4 +210,67 @@
             <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
         </p>
     </form>
+    <h3 id="upcloo-templating"><?php _e("Template selector", "wp_upcloo");?></h3>
+    <p class="warning">
+    	<?php _e("Use the advanced method only if you know or have someone that know CSS (Cascading StyleSheet)", "wp_upcloo"); ?>
+    </p>
+    <form method="post" action="options.php#upcloo-templating">
+        <?php wp_nonce_field('update-options'); ?>
+        <table class="form-table">
+            <tbody>
+            	<tr valign="top">
+                    <td width="92" scope="row"><?php echo _e("Select you preferrered method", "wp_upcloo");?></td>
+                    <td width="406">
+                        <input name="upcloo_template_base" type="radio" value="0" <?php echo ((get_option("upcloo_template_base", "wp_upcloo") == 0) ? 'checked="checked"' : '') ?>/>
+                        <strong><?php echo _e("(Default)");?></strong><br />
+                        <input name="upcloo_template_base" type="radio" value="1" <?php echo ((get_option("upcloo_template_base", "wp_upcloo") == 1) ? 'checked="checked"' : '') ?>/>
+                        <strong><?php echo _e("(Advanced)");?></strong>
+                    </td>
+                </tr>
+                <tr><td><h3><?php _e("Only if advanced is selected", "wp_upcloo");?></h3></td><td></td></tr>
+                <tr valign="top" style="border-top: 1px solid #f1f1f1;">
+                    <td width="92" scope="row"><?php echo _e("Show post title", "wp_upcloo");?></td>
+                    <td width="406">
+                    	<input name="upcloo_template_show_title" type="hidden" value="0" />
+                    	<input name="upcloo_template_show_title" type="checkbox" value="1" <?php echo ((get_option("upcloo_template_show_title", "wp_upcloo") == 1) ? 'checked="checked"' : '') ?>/>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <td width="92" scope="row"><?php echo _e("Show post featured image", "wp_upcloo");?></td>
+                    <td width="406">
+                    	<input name="upcloo_template_show_featured_image" type="hidden" value="0" />
+                    	<input name="upcloo_template_show_featured_image" type="checkbox" value="1" <?php echo ((get_option("upcloo_template_show_featured_image", "wp_upcloo") == 1) ? 'checked="checked"' : '') ?>/>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <td width="92" scope="row"><?php echo _e("Show post summary", "wp_upcloo");?></td>
+                    <td width="406">
+                    	<input name="upcloo_template_show_summary" type="hidden" value="0" />
+                    	<input name="upcloo_template_show_summary" type="checkbox" value="1" <?php echo ((get_option("upcloo_template_show_summary", "wp_upcloo") == 1) ? 'checked="checked"' : '') ?>/>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <td width="92" scope="row"><?php echo _e("Show post tags", "wp_upcloo");?></td>
+                    <td width="406">
+                    	<input name="upcloo_template_show_tags" type="hidden" value="0" />
+                    	<input name="upcloo_template_show_tags" type="checkbox" value="1" <?php echo ((get_option("upcloo_template_show_tags", "wp_upcloo") == 1) ? 'checked="checked"' : '') ?>/>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <td width="92" scope="row"><?php echo _e("Show post categories", "wp_upcloo");?></td>
+                    <td width="406">
+                    	<input name="upcloo_template_show_categories" type="hidden" value="0" />
+                    	<input name="upcloo_template_show_categories" type="checkbox" value="1" <?php echo ((get_option("upcloo_template_show_categories", "wp_upcloo") == 1) ? 'checked="checked"' : '') ?>/>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+            
+        <input type="hidden" name="action" value="update" />
+        <input type="hidden" name="page_options" value="upcloo_template_base, upcloo_template_show_title, upcloo_template_show_featured_image, upcloo_template_show_summary, upcloo_template_show_tags, upcloo_template_show_categories" />
+
+        <p class="submit">
+            <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+        </p>
+    </form>
 </div>
