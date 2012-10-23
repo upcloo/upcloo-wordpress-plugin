86fdf1f75e78d6662829d7080eec73845a3c0ea7
Walter Dal Mut <walter.dalmut@gmail.com>
2012-10-23 16:53:36 +0200

    Simplify the menu

--- wp-upcloo/options/app-config-options.php
+++ wp-upcloo/options/app-config-options.php
@@ -1,26 +1,7 @@
 <div class="wrap">
 <?php screen_icon("options-general")?>
 <h2><?php _e("UpCloo General Options", "wp_upcloo");?></h2>
-    <p>
-<?php _e("UpCloo is a new service created for web site with a lot of contents (from 10 to millions).", "wp_upcloo");?>
-    </p>
-    <p>
-<?php _e("If you manage contents on your web site (news, pages, reviews, products, comments...) then you're likely to spend a lot of time (and money!) to create correlations between different contents.", "wp_upcloo");?>
-    </p>
-    <p>
-<?php _e("UpCloo can index your pages and send, whenever a visitor goes on you web site, the best correlations at the moment through a simple xml feed, so you can show links and correlations inside or outside the text of your page.", "wp_upcloo");?>
-    </p>
-    <p>
-        <strong>
-<?php _e("Don't worry anymore about \"more like this\" and \"maybe you're interested at\": UpCloo manages all your correlations with a cloudy, smart and brilliant semantic engine.", "wp_upcloo");?>
-        </strong>
-    </p>
-    <div>
-    <h2><?php _e("UpCloo Security", "wp_upcloo");?></h2>
-        <?php _e("All information that you send to UpCloo Cloud System are secured using RSA 1024 bit.", "wp_upcloo");?>
-    </div>
-    <h2><?php _e("UpCloo Application Configuration", "wp_upcloo");?></h2>
-    <h3 id="upcloo-app-config"><?php _e("Login parmeters", "wp_upcloo");?></h3>
+    <h3 id="upcloo-app-config"><?php _e("Welcome To UpCloo", "wp_upcloo");?></h3>
     <form method="post" action="options.php#upcloo-app-config">
         <?php wp_nonce_field('update-options'); ?>
         <table class="form-table">
@@ -31,19 +12,48 @@
                     <input name="<?php echo UPCLOO_SITEKEY?>" type="text" value="<?php echo get_option(UPCLOO_SITEKEY, "wp_upcloo"); ?>" />
                     <strong><?php echo __("(Your signup info)")?></strong></td>
             </tr>
+            <tr valign="top">
+                    <th width="92" scope="row"><?php echo _e("Select Posts Type", "wp_upcloo");?></th>
+                    <td width="406">
+                    	<?php
+                    	    $postsType = get_post_types();
+                    	    $selected = get_option(UPCLOO_POSTS_TYPE);
+
+                    	    if (!is_array($selected)) {
+                    	        $selected = array();
+                    	    }
+                    	?>
+                    	<select name="<?php echo UPCLOO_POSTS_TYPE?>[]" multiple="multiple" size="6" style="width:380px;">
+                    		<?php
+                    		    foreach ($postsType as $key => $type):
+                    		?>
+                    		<option <?php echo ((in_array($key, $selected)) ? 'selected="selected"' : '')?> value="<?php echo $key?>"><?php echo $type?></option>
+                    		<?php
+                    		    endforeach;
+                    		?>
+                    	</select>
+                    </td>
+                </tr>
+            <tr valign="top">
+                <th width="92" scope="row"><?php _e("Max Number of Links", "wp_upcloo");?></th>
+                <td width="406">
+                    <?php $show_on_page = get_option(UPCLOO_MAX_SHOW_LINKS);?>
+                    <input name="<?php echo UPCLOO_MAX_SHOW_LINKS?>" type="text" value="<?php echo get_option(UPCLOO_MAX_SHOW_LINKS, ""); ?>" />
+                    <strong><?php _e("Let blank for all", "wp_uplcoo");?></strong></td>
+			</tr>
+			<tr valign="top">
+                <th width="92" scope="row"><?php _e("Rewrite public UpCloo label", "wp_upcloo");?></th>
+                <td width="406">
+                    <?php $show_on_page = get_option(UPCLOO_REWRITE_PUBLIC_LABEL);?>
+                    <input name="<?php echo UPCLOO_REWRITE_PUBLIC_LABEL?>" type="text" value="<?php echo get_option(UPCLOO_REWRITE_PUBLIC_LABEL, ""); ?>" />
+                    <strong><?php _e("Let blank for use default label (May be you are interested at)", "wp_uplcoo");?></strong></td>
+			</tr>
 
             <input type="hidden" name="action" value="update" />
-            <input type="hidden" name="page_options" value="<?php echo implode(",", array(UPCLOO_SITEKEY))?>" />
+            <input type="hidden" name="page_options" value="<?php echo implode(",", array(UPCLOO_SITEKEY, UPCLOO_POSTS_TYPE, UPCLOO_MAX_SHOW_LINKS, UPCLOO_REWRITE_PUBLIC_LABEL))?>" />
             </tbody>
         </table>
         <p class="submit">
-            <script type="text/javascript">
-                ;var confirmThat = function() {
-                    if (!confirm("Do you want to set this Sitekey?")) {
-                        return false;
-                    }
-                };
-            </script>
             <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" onclick="javascript:confirmThat()" />
         </p>
     </form>
--- wp-upcloo/wp-upcloo.php
+++ wp-upcloo/wp-upcloo.php
@@ -30,10 +30,6 @@ License: MIT
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
-//ini_set('error_reporting', E_ALL);
-//ini_set('display_errors', 'On');
-
 load_plugin_textdomain('wp_upcloo', null, basename(dirname(__FILE__)));
 
 require_once dirname(__FILE__) . '/UpCloo/Widget/Partner.php';
@@ -45,18 +41,9 @@ define("UPCLOO_SITEKEY", "upcloo_sitekey");
 define('UPCLOO_REWRITE_PUBLIC_LABEL', 'upcloo_rewrite_public_label');
 define('UPCLOO_MAX_SHOW_LINKS', "upcloo_max_show_links");
 define("UPCLOO_RSS_FEED", "http://www.mxdesign.it/contenuti/rss/0/news.xml");
-define('UPCLOO_ENABLE_MAIN_CORRELATION', "upcloo_enable_main_correlation");
-define('UPCLOO_DISABLE_MAIN_CORRELATION_COMPLETELY', "upcloo_disable_main_correlation_completely");
 define('UPCLOO_POSTS_TYPE', "upcloo_posts_type");
 
-define('UPCLOO_ENABLE_TEMPLATE_REMOTE_META', 'upcloo_enable_template_remote_meta');
-
-define('UPCLOO_SITEMAP_PAGE', 'upcloo_sitemap');
-
 define('UPCLOO_MENU_SLUG', 'upcloo_options_menu');
-define('UPCLOO_MENU_FEATURE_SLUG', 'upcloo_options_menu_feature');
-define('UPCLOO_MENU_POST_TYPE_SLUG', 'upcloo_options_menu_post_type');
-define('UPCLOO_MENU_REMOTE', 'upcloo_options_menu_remote');
 
 define('UPCLOO_VIEW_PATH', dirname(__FILE__) . '/views');
 
@@ -79,14 +66,6 @@ register_activation_hook(__FILE__, 'upcloo_install');
 /* Runs on plugin deactivation*/
 register_deactivation_hook(__FILE__, 'upcloo_remove');
 
-//If have to show meta values
-if (get_option(UPCLOO_ENABLE_TEMPLATE_REMOTE_META, "wp_upcloo")) {
-    //Add sitemap page
-    if (array_key_exists("plugin_page", $_GET) && $_GET['plugin_page'] == UPCLOO_SITEMAP_PAGE) {
-        add_action('template_redirect', 'upcloo_sitemap_page');
-    }
-}
-
 function upcloo_is_configured()
 {
     $postTypes = get_option(UPCLOO_POSTS_TYPE);
@@ -108,8 +87,7 @@ function upcloo_show_needs_attention()
 {
     if (!upcloo_is_configured()) {
         echo '<div class="updated">
-        <p>' . __("Remember that your have to configure UpCloo Plugin: ", "wp_upcloo") . ' <a href="admin.php?page=upcloo_options_menu">'.__("Base Config Page", "wp_upcloo") . '</a> - <a href="admin.php?page=upcloo_options_menu_post_type">'. __("Content Types Selection", "wp_upcloo") . '</a></p>
-        </div>';
+        <p>' . __("Remember that your have to configure UpCloo Plugin: ", "wp_upcloo") . ' <a href="admin.php?page=upcloo_options_menu">'.__("Base Config Page", "wp_upcloo") . '</a> - <a href="admin.php?page=upcloo_options_menu_post_type">'. __("Content Types Selection", "wp_upcloo") . '</a></p></div>';
     }
 }
 
@@ -124,27 +102,6 @@ function upcloo_check_menu_capability()
 function upcloo_plugin_menu()
 {
     add_menu_page('UpCloo', __('UpCloo', "wp_upcloo"), UPCLOO_OPTION_CAPABILITY, UPCLOO_MENU_SLUG, 'upcloo_plugin_options');
-    add_submenu_page(UPCLOO_MENU_SLUG, "UpCloo Post Type", __("Post Type Indexing", "wp_upcloo"), UPCLOO_OPTION_CAPABILITY, UPCLOO_MENU_POST_TYPE_SLUG, 'upcloo_plugin_menu_post_type');
-    add_submenu_page(UPCLOO_MENU_SLUG, "UpCloo Indexing Feature", __("Indexing Feature", "wp_upcloo"), UPCLOO_OPTION_CAPABILITY, UPCLOO_MENU_FEATURE_SLUG, 'upcloo_plugin_menu_feature');
-    add_submenu_page(UPCLOO_MENU_SLUG, "UpCloo Remote Importer", __("Remote Importer", "wp_upcloo"), UPCLOO_OPTION_CAPABILITY, UPCLOO_MENU_REMOTE_SLUG, 'upcloo_plugin_menu_remote');
-}
-
-function upcloo_plugin_menu_remote()
-{
-    upcloo_check_menu_capability();
-    include realpath(dirname(__FILE__)) . "/options/app-remote-options.php";
-}
-
-function upcloo_plugin_menu_post_type()
-{
-    upcloo_check_menu_capability();
-    include realpath(dirname(__FILE__)) . "/options/app-post-type-options.php";
-}
-
-function upcloo_plugin_menu_feature()
-{
-    upcloo_check_menu_capability();
-    include realpath(dirname(__FILE__)) . "/options/app-feature-options.php";
 }
 
 function upcloo_plugin_options()
@@ -153,105 +110,6 @@ function upcloo_plugin_options()
     include realpath(dirname(__FILE__)) . "/options/app-config-options.php";
 }
 //End menu
-
-/**
- * Generate a sitemap for UpCloo Remote Importer
- */
-function upcloo_sitemap_page()
-{
-    header ("content-type: text/xml");
-
-    $view = new SView();
-    $view->setViewPath(UPCLOO_VIEW_PATH);
-
-    $view->userSelected = get_option(UPCLOO_POSTS_TYPE);
-
-    echo $view->render("sitemap.phtml");
-    exit;
-}
-
-
-/**
- * Use only in single.php
- *
- * Call this function only in single.php theme file
- *
- * @return string The content to attach into head.
- */
-function upcloo_wp_head()
-{
-    $metas = '';
-
-    if (get_option(UPCLOO_ENABLE_TEMPLATE_REMOTE_META, "wp_upcloo")) {
-
-        $postTypes = get_option(UPCLOO_POSTS_TYPE);
-        if (!is_array($postTypes)) {
-            $postTypes = array();
-        }
-
-
-        if (is_single()) {
-            $m = array();
-
-            global $post;
-
-            //TODO: refactor...
-            if (!in_array($post->post_type, $postTypes)) {
-                return;
-            }
-
-            $publish_date = $post->post_date;
-            $publish_date = str_replace(" ", "T", $publish_date) . "Z";
-
-            $m[] = '<!-- UPCLOO_POST_ID '.$post->post_type . "_" . $post->ID.' UPCLOO_POST_ID -->';
-            $m[] = '<!-- UPCLOO_POST_TYPE '.$post->post_type.' UPCLOO_POST_TYPE -->';
-            $m[] = '<!-- UPCLOO_POST_TITLE '.$post->post_title.' UPCLOO_POST_TITLE -->';
-            $m[] = '<!-- UPCLOO_POST_PUBLISH_DATE '.$publish_date.' UPCLOO_POST_PUBLISH_DATE -->';
-
-            $image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'thumbnail');
-            if ($image) {
-            	if (is_array($image)) {
-            		$image = $image[0];
-            	}
-                $m[] = '<!-- UPCLOO_POST_IMAGE '.$image.' UPCLOO_POST_IMAGE -->';
-            }
-
-            $firstname = get_user_meta($post->post_author, "first_name", true);
-            $lastname = get_user_meta($post->post_author, "last_name", true);
-
-            $m[] = '<!-- UPCLOO_POST_AUTHOR '.$firstname . " " . $lastname .' UPCLOO_POST_AUTHOR -->';
-
-            //LANG!
-            $lang = get_post_meta($post->ID, UPCLOO_META_LANG, true);
-            if ($lang && !empty($lang)) {
-                $m[] = '<!-- UPCLOO_POST_LANG ' . $lang . ' UPCLOO_POST_LANG -->';
-            }
-
-            $tags = get_the_tags($post->ID);
-            if (is_array($tags)) {
-                $elements = array();
-                foreach ($tags as $element) {
-                    $elements[] = $element->name;
-                }
-                $m[] = '<!-- UPCLOO_POST_TAGS '.implode(",", $elements).' UPCLOO_POST_TAGS -->';
-            }
-
-            $categories = get_the_category($post->ID);
-            if (is_array($categories)) {
-                $elements = array();
-                foreach ($categories as $element) {
-                    $elements[] = $element->name;
-                }
-                $m[] = '<!-- UPCLOO_POST_CATEGORIES '.implode(",", $elements).' UPCLOO_POST_CATEGORIES -->';
-            }
-
-
-            $metas = implode(PHP_EOL, $m) . PHP_EOL;
-        }
-    }
-
-    echo $metas;
-}
 
 // Create the function to output the contents of our Dashboard Widget
 function upcloo_dashboard_widget_function()
@@ -296,11 +154,8 @@ function upcloo_install() {
     /* Creates new database field */
     add_option(UPCLOO_SITEKEY, "", "", "yes");
     add_option(UPCLOO_MAX_SHOW_LINKS, "5", "", "yes");
-    add_option(UPCLOO_ENABLE_MAIN_CORRELATION, "1", "", "yes");
-    add_option(UPCLOO_DISABLE_MAIN_CORRELATION_COMPLETELY, '0', '', 'yes');
     add_option(UPCLOO_POSTS_TYPE, array("post"), '', 'yes');
     add_option(UPCLOO_REWRITE_PUBLIC_LABEL, '','', 'yes');
-    add_option(UPCLOO_ENABLE_TEMPLATE_REMOTE_META, '0', '', 'yes');
 }
 
 /**
@@ -312,11 +167,8 @@ function upcloo_install() {
 function upcloo_remove() {
     /* Deletes the database field */
     delete_option(UPCLOO_MAX_SHOW_LINKS);
-    delete_option(UPCLOO_ENABLE_MAIN_CORRELATION);
-    delete_option(UPCLOO_DISABLE_MAIN_CORRELATION_COMPLETELY);
     delete_option(UPCLOO_REWRITE_PUBLIC_LABEL);
     delete_option(UPCLOO_POSTS_TYPE);
-    delete_option(UPCLOO_ENABLE_TEMPLATE_REMOTE_META);
 }
 
 /**
@@ -346,39 +198,14 @@ function upcloo_content($content, $noPostBody = false)
         $postTypes = array();
     }
 
-    if (get_option(UPCLOO_ENABLE_TEMPLATE_REMOTE_META, "wp_upcloo")) {
-        //TODO: this part is written twice... clear...
-        if ((is_single($post) && (in_array($post->post_type, $postTypes)))) {
-            $content = "<!-- UPCLOO_POST_CONTENT -->{$content}<!-- UPCLOO_POST_CONTENT -->";
-        }
-    }
-
-    $original = $content;
-
-    if (get_option(UPCLOO_DISABLE_MAIN_CORRELATION_COMPLETELY) == "1") {
-        return $content;
-    }
-
-    /**
-     * Check if the content is single
-     *
-     * @todo refactor this check
-     * Use a filter login to perform this kind of selection
-     *
-     * Check if UpCloo is enabled
-     */
-    if (
-        (is_single($post) && (in_array($post->post_type, $postTypes)))
-        &&
-        (get_option(UPCLOO_ENABLE_MAIN_CORRELATION) || (!get_option(UPCLOO_ENABLE_MAIN_CORRELATION) && $current_user->has_cap('edit_users'))))
-    {
+    if (is_single($post) && (in_array($post->post_type, $postTypes))) {
         $view = new SView();
         $view->setViewPath(UPCLOO_VIEW_PATH);
 
         $view->permalink = get_permalink($post->ID);
         $view->sitekey = get_option(UPCLOO_SITEKEY);
         $view->headline = (!(get_option(UPCLOO_REWRITE_PUBLIC_LABEL)) || trim(get_option(UPCLOO_REWRITE_PUBLIC_LABEL)) == '')
-            ? __("Maybe you are interested at", "wp_upcloo")
+            ? __("Maybe you are also interested in", "wp_upcloo")
             :  get_option(UPCLOO_REWRITE_PUBLIC_LABEL);
 
 
@@ -387,25 +214,5 @@ function upcloo_content($content, $noPostBody = false)
 
 
     return $content;
-}
-
-/**
- * Get base domain path of an url
- *
- * @param string $url
- * @return boolean The base url in case of success, false otherwise.
- */
-function upcloo_is_external_site($url)
-{
-    $urlSchema = @parse_url($url);
-    if ($urlSchema) {
-        if ($urlSchema["host"] == $_SERVER["SERVER_NAME"]) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    return true;
 }
 
